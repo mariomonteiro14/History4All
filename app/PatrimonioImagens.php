da826840ebab04b42b0ec95ff0f97841f37d1746
tigmmp0 <2160880@my.ipleiria.pt>
2019-03-22 18:34:51 +0000

    Modelo

---
+++ app/PatrimonioImagens.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class PatrimonioImagens extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'patrimonio_imagens';
+
+    protected $fillable = [
+        'foto'
+    ];
+
+}
---
+++ app/Turma.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Turma extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'nome', 'escola_id'
+    ];
+
+}
