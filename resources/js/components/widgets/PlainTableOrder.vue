5d2cf0a8116630998d208cbf4c134c633a771d34
Mário Monteiro <2160799@my.ipleiria.pt>
2019-03-29 20:19:04 +0000

    filtros patrimonios - apenas desenho

--- resources/js/components/widgets/PlainTableOrder.vue
+++ resources/js/components/widgets/PlainTableOrder.vue
@@ -6,21 +6,44 @@
 
         <v-card append float>
             <v-card-title>
-                <v-select
-                    :items="patrimonios"
-                    label="Distrito"
-                    @change="filterDistrito"
-                ></v-select>
-                <v-spacer></v-spacer>
-                <v-text-field
-                    v-model="search"
-                    append-icon="search"
-                    label="Search"
-                    single-line
-                    hide-details
-                ></v-text-field>
+
+                <v-layout align-center wrap>
+                    <v-select
+                        :items="patrimonios"
+                        label="Distrito"
+                        @change="filterDistrito"
+                    ></v-select>
+                    <v-spacer></v-spacer>
+                    <v-select
+                        v-model="epoca"
+                        :items="patrimonios"
+                        attach
+                        chips
+                        label="Epoca"
+                        multiple
+                    ></v-select>
+                    <v-spacer></v-spacer>
+                    <v-select
+                        v-model="ciclo"
+                        :items="patrimonios"
+                        attach
+                        chips
+                        label="Ciclos"
+                        multiple
+                    ></v-select>
+                    <v-spacer></v-spacer>
+
+                    <v-text-field
+                        v-model="search"
+                        append-icon="search"
+                        label="Search"
+                        single-line
+                        hide-details
+                    ></v-text-field>
+                </v-layout>
             </v-card-title>
-            <v-data-table :headers="headers" :items="patrimonios" :search="search">
+            <v-data-table :headers="headers" :items="patrimonios" :search="search" class="elevation-1"
+                          :pagination.sync="pagination">
 
                 <template v-slot:items="props">
                     <td class="text-xs-left">{{ }}</td>
@@ -46,6 +69,14 @@
         },
         data() {
             return {
+                pagination: {
+                    descending: true,
+                    page: 1,
+                    rowsPerPage: 5,
+                    sortBy: 'fat',
+                    totalItems: 0,
+                    rowsPerPageItems: [5, 10, 20]
+                },
                 search: '',
                 headers: [
                     {
