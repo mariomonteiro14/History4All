13b63401268efa623df1ff37bd64b4ed4142fcdc
tigmmp0 <2160880@my.ipleiria.pt>
2019-03-31 20:18:52 +0100

    middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\IsAdmin::class,
+        'aluno' => \App\Http\Middleware\IsAluno::class,
+        'professor' => \App\Http\Middleware\IsProfessor::class,
     ];
 
     /**
