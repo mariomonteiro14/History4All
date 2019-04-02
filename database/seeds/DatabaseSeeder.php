41d705e2a9ec42119d75f8c0c511a0842a54f326
Mário Monteiro <2160799@my.ipleiria.pt>
2019-04-02 17:36:01 +0100

    nova interface criar patrimonio - melhoramento

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         //$this->call(UsersSeeder::class);
-         //$this->call(PatrimoniosSeeder::class);
-         //$this->call(PatrimonioImagesSeeder::class);
+         $this->call(UsersSeeder::class);
+         $this->call(PatrimoniosSeeder::class);
+         $this->call(PatrimonioImagesSeeder::class);
     }
 }
